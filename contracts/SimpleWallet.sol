3b5027dc23e25cc8ef969f69a7f59a67bc296846
Swarnab Garang <swarnabgarang@gmail.com>
2021-12-30 15:31:12 +0530

    added events for SimpleWallet contract

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -33,17 +33,19 @@ contract Allowance is Ownable {
 
 contract SimpleWallet is Allowance {
 
-    
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
 
     function withDrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         require(_amount <= address(this).balance, "Not enough funds in Smart Contract");
         if (!isOwner()) {
             reduceAllowance(msg.sender, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
     receive () external payable {
-
+        emit MoneyReceived(msg.sender, msg.value);
     }
 }
