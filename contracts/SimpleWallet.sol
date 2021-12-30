ac54e9b37c12a7bd5fbf36f589054c77fe1a2af2
Swarnab Garang <swarnabgarang@gmail.com>
2021-12-30 13:44:53 +0530

    added allowance feature

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -6,7 +6,22 @@ pragma solidity ^0.8.11;
 
 contract SimpleWallet is Ownable {
 
-    function withDrawMoney(address payable _to, uint _amount) public onlyOwner {
+    function isOwner() internal view returns(bool) {
+        return owner() == msg.sender;
+    }
+
+    mapping(address => uint) public allowance;
+
+    function addAllowance(address _who, uint _amount) public onlyOwner{
+        allowance[_who] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[msg.sender] >= _amount, "Allowance could not be verified");
+        _;
+    }
+
+    function withDrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         _to.transfer(_amount);
     }
 
