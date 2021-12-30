2fe3bab68bb5ab47544260f645b62e9cef024753
Swarnab Garang <swarnabgarang@gmail.com>
2021-12-30 13:07:12 +0530

    added modifier for only owner access

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -1,14 +1,26 @@
 //SPDX-License-Identifier: MIT
 
+
 pragma solidity ^0.8.11;
 
 contract SimpleWallet {
 
-    function withDrawMoney(address payable _to, uint _amount) public {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Owner could not be verified");
+        _;
+    }
+
+    function withDrawMoney(address payable _to, uint _amount) public onlyOwner {
         _to.transfer(_amount);
     }
 
-    fallback () external payable {
+    receive () external payable {
 
     }
 }
