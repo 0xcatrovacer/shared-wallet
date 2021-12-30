fe40c88888b4f740423eeabc3cf7988d0b1838fd
Swarnab Garang <swarnabgarang@gmail.com>
2021-12-30 15:45:56 +0530

    added Allowance contract in its own file

---
+++ contracts/Allowance.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: MIT
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+pragma solidity ^0.8.11;
+
+contract Allowance is Ownable {
+
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
+
+    function isOwner() internal view returns(bool) {
+        return owner() == msg.sender;
+    }
+
+    mapping(address => uint) public allowance;
+
+    function addAllowance(address _who, uint _amount) public onlyOwner{
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+        allowance[_who] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[msg.sender] >= _amount, "Allowance could not be verified");
+        _;
+    }
+
+    function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
+        allowance[_who] -= _amount;
+    }
+
+}
